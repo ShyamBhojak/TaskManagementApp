c4a060082075293ce6fec703bfbcbf1ba455534d
H.B.INSTITUTE - THE TRAINING CENTER <hbinstitute@H.B.INSTITUTE>
2026-08-26 20:14:00 +0530

    Configuration 1. Connection String 2. Settings with the help of pydantic_settings 3. Configured db.py for Base, Engine and LocalSession 4. And finally added to main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
+from src.utils.db import Base, engine
 
-app = FastAPI(title="Task Management App",description="Task Management App System by FastAPI")
+Base.metadata.create_all(engine)
+
+app = FastAPI(
+    title="Task Management App",
+    description="Task Management App System by FastAPI",
+    version="1.0.0"
+)
+
+
